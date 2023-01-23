3887fd6ef1a759c38f8c68ec8e22d0b453312f7a
Mbazu Daniel <Mbazudaniel97@gmail.com>
2023-01-23 08:12:46 +0100

    update Hero background gradient

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY} `}
+      className={`flex md:flex-row flex-col ${styles.paddingY}  `}
     >
       <div className={`flex-1 flex-row items-center xl:px-0 sm:px-16 px-6 `}>
         <div className="flex flex-row justify-between items-center w-full">
@@ -33,22 +33,26 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-0 `}>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-0  `}>
         {/* <div
           className={`${xStyle.bgIntro} mx-auto order-last md:order-3  overflow-hidden `}
         > */}
-          <div className="w-[90%] flex justify-center m-0 p-0">
+          <div className="w-[90%] flex justify-center m-0 p-0 ">
             <Image
-              className="object-cover w-[100%] h-[100%]"
+              className="object-contain w-[100%] h-[100%] z-[5]"
               src={wealthmate}
               alt="banking"
             />
+
+<div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient " />
+    <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40  white__gradient " />
+    <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient " />
           </div>
         {/* </div> */}
 
-        <div className={`ss:hidden ${styles.flexCenter}`}>
+        {/* <div className={`hidden ${styles.flexCenter}`}>
           <GetStarted />
-        </div>
+        </div> */}
       </div>
     </section>
   );
